ffdc0d849b6cdf53bde26809bf67483d0de861fb
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Move utils for errors handling to a single file

--- runtime/errors.go
+++ runtime/errors.go
@@ -5,3 +5,17 @@ import "github.com/pkg/errors"
 func newUnknownStateError(state string) error {
 	return errors.Errorf("unknown state %s", state)
 }
+
+// ErrorHandler ...
+//go:generate mockery -name=ErrorHandler -case=underscore
+type ErrorHandler interface {
+	HandleError(err error)
+}
+
+// DefaultErrorHandler ...
+type DefaultErrorHandler struct{}
+
+// HandleError ...
+func (handler DefaultErrorHandler) HandleError(err error) {
+	panic(err)
+}
--- runtime/errors_test.go
+++ runtime/errors_test.go
@@ -2,6 +2,7 @@ package runtime
 
 import (
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,4 +10,30 @@ import (
 func TestNewUnknownStateError(test *testing.T) {
 	got := newUnknownStateError("test")
 	assert.Equal(test, "unknown state test", got.Error())
+}
+
+func TestDefaultErrorHandler_HandleError(test *testing.T) {
+	type args struct {
+		err error
+	}
+
+	for _, testData := range []struct {
+		name      string
+		args      args
+		wantPanic func(assert.TestingT, assert.PanicTestFunc, ...interface{}) bool
+	}{
+		{
+			name:      "nil error",
+			wantPanic: assert.NotPanics,
+		},
+		{
+			name:      "not nil error",
+			args:      args{iotest.ErrTimeout},
+			wantPanic: assert.Panics,
+		},
+	} {
+		test.Run(testData.name, func(test *testing.T) {
+			testData.wantPanic(test, func() { DefaultErrorHandler{}.HandleError(testData.args.err) })
+		})
+	}
 }
