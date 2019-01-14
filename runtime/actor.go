ecb9ed3217762f3d5714123f56b81025b479fe3a
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the runtime.Actor.SetState() method

--- runtime/actor.go
+++ runtime/actor.go
@@ -5,3 +5,14 @@ type Actor struct {
 	currentState string
 	states       StateGroup
 }
+
+// SetState changes a current state of an actor.
+// If a new state isn't contained in a state map of the actor, it'll cause an error.
+func (actor *Actor) SetState(state string) error {
+	if _, ok := actor.states[state]; !ok {
+		return newUnknownStateError(state)
+	}
+
+	actor.currentState = state
+	return nil
+}
