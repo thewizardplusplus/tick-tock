896595ddd4cc3b846a1f60c96bd5ae6898076881
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the interpreter.Interpret() function

---
+++ interpreter/interpreter.go
@@ -0,0 +1,41 @@
+package interpreter
+
+import (
+	"github.com/thewizardplusplus/tick-tock/parser"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/translator"
+)
+
+// Dependencies ...
+type Dependencies struct {
+	translator.Dependencies
+	ReaderDependencies
+}
+
+// Interpret ...
+func Interpret(
+	context context.Context,
+	filename string,
+	inboxSize int,
+	dependencies Dependencies,
+) error {
+	code, err := readCode(filename, dependencies.ReaderDependencies)
+	if err != nil {
+		return err
+	}
+
+	program, err := parser.Parse(code)
+	if err != nil {
+		return err
+	}
+
+	actors, err := translator.Translate(program.Actors, inboxSize, dependencies.Dependencies)
+	if err != nil {
+		return err
+	}
+
+	actors.Start(context)
+	actors.SendMessage("__initialization__")
+
+	return nil
+}
