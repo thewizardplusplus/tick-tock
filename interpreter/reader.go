88b67af7af5645a19c654dc7cd1419eb07fb6c75
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the interpreter.readCode() function

--- interpreter/reader.go
+++ interpreter/reader.go
@@ -1,5 +1,49 @@
 package interpreter
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/afero"
+)
+
+// ReaderDependencies ...
+type ReaderDependencies struct {
+	DefaultReader io.Reader
+	FileSystem    afero.Fs
+}
+
+func readCode(filename string, dependencies ReaderDependencies) (string, error) {
+	var reader io.Reader
+	if isEmptyFilename(filename) {
+		reader = dependencies.DefaultReader
+	} else {
+		file, err := dependencies.FileSystem.Open(filename)
+		if err != nil {
+			return "", errors.Wrapf(err, "unable to open the file %s", filename)
+		}
+		defer file.Close()
+
+		reader = file
+	}
+
+	code, err := ioutil.ReadAll(reader)
+	if err != nil {
+		var message string
+		if isEmptyFilename(filename) {
+			message = "unable to read the default source"
+		} else {
+			message = fmt.Sprintf("unable to read the file %s", filename)
+		}
+
+		return "", errors.Wrap(err, message)
+	}
+
+	return string(code), nil
+}
+
 func isEmptyFilename(filename string) bool {
 	return len(filename) == 0 || filename == "-"
 }
