d8a45b80f64d3e63a714fb0fdd8004022a792679
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the runtime.CommandGroup type

--- runtime/command.go
+++ runtime/command.go
@@ -1,6 +1,23 @@
 package runtime
 
+import "github.com/pkg/errors"
+
 // Command is implemented by any command. Real type doesn't have to be pure and can store a state.
 type Command interface {
 	Run() error
+}
+
+// CommandGroup represents a list of commands in a certain order.
+type CommandGroup []Command
+
+// Run executes commands sequentially one by one.
+// If any command fails, execution stops and the error is returned.
+func (commands CommandGroup) Run() error {
+	for index, command := range commands {
+		if err := command.Run(); err != nil {
+			return errors.Wrapf(err, "unable to run the command #%d", index)
+		}
+	}
+
+	return nil
 }
