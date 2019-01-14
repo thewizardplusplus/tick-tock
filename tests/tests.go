69b292f31db06aa3a84893382e434e4ed4fece2a
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the tests.Exiter interface

--- tests/tests.go
+++ tests/tests.go
@@ -8,6 +8,12 @@ type Writer interface {
 	io.Writer
 }
 
+// Exiter ...
+//go:generate mockery -name=Exiter -case=underscore
+type Exiter interface {
+	Exit(code int)
+}
+
 // GetAddress ...
 func GetAddress(s string) *string {
 	return &s
