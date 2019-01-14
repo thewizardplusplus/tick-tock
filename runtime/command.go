23c574da36aa60edd85f6f6a99e96e315a3efbe2
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the runtime.Context interface

--- runtime/command.go
+++ runtime/command.go
@@ -2,6 +2,13 @@ package runtime
 
 import "github.com/pkg/errors"
 
+// Context ...
+//go:generate mockery -name=Context -inpkg -case=underscore -testonly
+type Context interface {
+	SendMessage(message string)
+	SetState(state string) error
+}
+
 // Command ...
 //go:generate mockery -name=Command -inpkg -case=underscore -testonly
 type Command interface {
