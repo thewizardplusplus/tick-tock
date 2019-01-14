15f466dc31db74f5f8a7cea6225a0676b8443252
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the main.main() function

---
+++ cmd/tick-tock/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"sync"
+
+	"github.com/spf13/afero"
+	"github.com/thewizardplusplus/tick-tock/interpreter"
+	"github.com/thewizardplusplus/tick-tock/runtime"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/translator"
+)
+
+func main() {
+	options := parseOptions()
+	waiter := new(sync.WaitGroup)
+	errorHandler := runtime.NewDefaultErrorHandler(os.Stderr, os.Exit)
+	dependencies := interpreter.Dependencies{
+		Dependencies: translator.Dependencies{
+			Dependencies: runtime.Dependencies{Waiter: waiter, ErrorHandler: errorHandler},
+			OutWriter:    os.Stdout,
+		},
+		ReaderDependencies: interpreter.ReaderDependencies{
+			DefaultReader: os.Stdin,
+			FileSystem:    afero.NewOsFs(),
+		},
+	}
+	if err := interpreter.Interpret(
+		new(context.DefaultContext),
+		options.filename,
+		options.inboxSize,
+		dependencies,
+	); err != nil {
+		errorHandler.HandleError(err)
+	}
+
+	waiter.Wait()
+}
