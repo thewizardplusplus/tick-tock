a50417a115326122fd0483b6a9131222628b1757
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Extract interfaces of the tests package to a single file

---
+++ internal/tests/interfaces.go
@@ -0,0 +1,37 @@
+package tests
+
+import (
+	"io"
+
+	"github.com/spf13/afero"
+)
+
+// Reader ...
+//go:generate mockery -name=Reader -case=underscore
+type Reader interface {
+	io.Reader
+}
+
+// Writer ...
+//go:generate mockery -name=Writer -case=underscore
+type Writer interface {
+	io.Writer
+}
+
+// FileSystem ...
+//go:generate mockery -name=FileSystem -case=underscore
+type FileSystem interface {
+	afero.Fs
+}
+
+// File ...
+//go:generate mockery -name=File -case=underscore
+type File interface {
+	afero.File
+}
+
+// Exiter ...
+//go:generate mockery -name=Exiter -case=underscore
+type Exiter interface {
+	Exit(code int)
+}
--- internal/tests/tests.go
+++ internal/tests/tests.go
@@ -1,10 +1,8 @@
 package tests
 
 import (
-	"io"
 	"sync"
 
-	"github.com/spf13/afero"
 	"github.com/thewizardplusplus/tick-tock/runtime/waiter"
 )
 
@@ -13,36 +11,6 @@ const (
 	UnbufferedInbox = iota
 	BufferedInbox
 )
-
-// Reader ...
-//go:generate mockery -name=Reader -case=underscore
-type Reader interface {
-	io.Reader
-}
-
-// Writer ...
-//go:generate mockery -name=Writer -case=underscore
-type Writer interface {
-	io.Writer
-}
-
-// FileSystem ...
-//go:generate mockery -name=FileSystem -case=underscore
-type FileSystem interface {
-	afero.Fs
-}
-
-// File ...
-//go:generate mockery -name=File -case=underscore
-type File interface {
-	afero.File
-}
-
-// Exiter ...
-//go:generate mockery -name=Exiter -case=underscore
-type Exiter interface {
-	Exit(code int)
-}
 
 // SynchronousWaiter ...
 type SynchronousWaiter struct {
