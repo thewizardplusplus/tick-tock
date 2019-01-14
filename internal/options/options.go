bce5b31f2dc211e6fc5f3904134cad95fa2034d5
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-14 03:47:02 +0300

    Add the options.Parse() function

---
+++ internal/options/options.go
@@ -0,0 +1,59 @@
+package options
+
+import (
+	"io"
+	"path/filepath"
+	"strconv"
+
+	"github.com/thewizardplusplus/tick-tock/interpreter"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+// ...
+const (
+	Version = "v1.0"
+
+	DefaultInboxSize      = 10
+	DefaultInitialState   = "__initialization__"
+	DefaultInitialMessage = "__initialize__"
+)
+
+// Dependencies ...
+type Dependencies struct {
+	UsageWriter io.Writer
+	ErrorWriter io.Writer
+	Exiter      func(code int)
+}
+
+// Parse ...
+func Parse(args []string, dependencies Dependencies) (interpreter.Options, error) {
+	app := kingpin.New(filepath.Base(args[0]), "")
+	app.UsageWriter(dependencies.UsageWriter)
+	app.ErrorWriter(dependencies.ErrorWriter)
+	app.Terminate(dependencies.Exiter)
+
+	app.Version(Version)
+	app.VersionFlag.Short('v')
+	app.HelpFlag.Short('h')
+
+	var options interpreter.Options
+	app.Flag("inbox", "Inbox buffer size.").
+		Short('i').
+		Default(strconv.Itoa(DefaultInboxSize)).
+		IntVar(&options.InboxSize)
+	app.Flag("state", "Initial state.").
+		Short('s').
+		Default(DefaultInitialState).
+		StringVar(&options.InitialState)
+	app.Flag("message", "Initial message.").
+		Short('m').
+		Default(DefaultInitialMessage).
+		StringVar(&options.InitialMessage)
+	app.Arg("filename", `Source file name. Empty or "-" means stdin.`).StringVar(&options.Filename)
+
+	if _, err := app.Parse(args[1:]); err != nil {
+		return interpreter.Options{}, err
+	}
+
+	return options, nil
+}
