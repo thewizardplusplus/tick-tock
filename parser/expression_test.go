5e10725a840890be67a11d3d13d22b9f7414e411
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-01-16 23:00:00 +0300

    Add a base tests for the parser.parseToAST() function with the parser.Expression structure

---
+++ parser/expression_test.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseToAST_withExpression(test *testing.T) {
+	type args struct {
+		code string
+		ast  interface{}
+	}
+
+	for _, testData := range []struct {
+		name    string
+		args    args
+		wantAST interface{}
+		wantErr assert.ErrorAssertionFunc
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(testData.name, func(test *testing.T) {
+			err := parseToAST(testData.args.code, testData.args.ast)
+			assert.Equal(test, testData.wantAST, testData.args.ast)
+			testData.wantErr(test, err)
+		})
+	}
+}
