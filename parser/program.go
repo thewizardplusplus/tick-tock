a395e2d2157f587a26d0b4190a0f23a9eab8ffb6
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-01-16 23:00:00 +0300

    Move the parser.Program structure to a separate file

---
+++ parser/program.go
@@ -0,0 +1,38 @@
+package parser
+
+// Program ...
+type Program struct {
+	Actors []*Actor `parser:"{ @@ }"`
+}
+
+// Actor ...
+type Actor struct {
+	States []*State `parser:"\"actor\" { @@ } \";\""`
+}
+
+// State ...
+type State struct {
+	Name     string     `parser:"\"state\" @Ident"`
+	Messages []*Message `parser:"{ @@ } \";\""`
+}
+
+// Message ...
+type Message struct {
+	Name     string     `parser:"\"message\" @Ident"`
+	Commands []*Command `parser:"{ @@ } \";\""`
+}
+
+// Command ...
+type Command struct {
+	Send  *string       `parser:"\"send\" @Ident"`
+	Set   *string       `parser:"| \"set\" @Ident"`
+	Out   *string       `parser:"| \"out\" ( @String | @RawString )"`
+	Sleep *SleepCommand `parser:"| @@"`
+	Exit  bool          `parser:"| @\"exit\""`
+}
+
+// SleepCommand ...
+type SleepCommand struct {
+	Minimum *float64 `parser:"\"sleep\" ( @Int | @Float )"`
+	Maximum *float64 `parser:"\",\" ( @Int | @Float )"`
+}
