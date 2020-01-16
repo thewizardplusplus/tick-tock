e4ffb85fd24117c39e5736b4778f65e47723235b
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-01-16 23:00:00 +0300

    Implement tests of the parser.parseToAST() function with the parser.Atom structure

--- parser/expression_test.go
+++ parser/expression_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/thewizardplusplus/tick-tock/internal/tests"
 )
 
 func TestParseToAST_withExpression(test *testing.T) {
@@ -18,7 +19,24 @@ func TestParseToAST_withExpression(test *testing.T) {
 		wantAST interface{}
 		wantErr assert.ErrorAssertionFunc
 	}{
-		// TODO: add test cases
+		{
+			name:    "Atom/number/integer",
+			args:    args{"23", new(Atom)},
+			wantAST: &Atom{Number: tests.GetNumberAddress(23)},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "Atom/number/floating-point",
+			args:    args{"2.3", new(Atom)},
+			wantAST: &Atom{Number: tests.GetNumberAddress(2.3)},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "Atom/identifier",
+			args:    args{"test", new(Atom)},
+			wantAST: &Atom{Identifier: tests.GetStringAddress("test")},
+			wantErr: assert.NoError,
+		},
 	} {
 		test.Run(testData.name, func(test *testing.T) {
 			err := parseToAST(testData.args.code, testData.args.ast)
