74bc76cf4fe22cb2114390873c8dca46cdcee21a
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-02-08 23:00:00 +0300

    Add the expressions.NewSubtraction() function

--- runtime/expressions/functions_applyings.go
+++ runtime/expressions/functions_applyings.go
@@ -43,3 +43,13 @@ func NewAddition(leftOperand Expression, rightOperand Expression) FunctionApplyi
 		}),
 	)
 }
+
+// NewSubtraction ...
+func NewSubtraction(leftOperand Expression, rightOperand Expression) FunctionApplying {
+	return NewFunctionApplying(
+		[]Expression{leftOperand, rightOperand},
+		NewArithmeticFunctionHandler(func(arguments []float64) (float64, error) {
+			return arguments[0] - arguments[1], nil
+		}),
+	)
+}
