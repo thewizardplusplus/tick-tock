e9e78ba1c286d34ba6dd8a463a490f509c729e34
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-02-14 23:00:00 +0300

    Restore the expressions.FunctionCall structure

--- runtime/expressions/function_call.go
+++ runtime/expressions/function_call.go
@@ -7,44 +7,59 @@ import (
 	"github.com/thewizardplusplus/tick-tock/runtime/context"
 )
 
+// FunctionCall ...
+type FunctionCall struct {
+	name      string
+	arguments []Expression
+}
+
 // NewFunctionCall ...
-func NewFunctionCall(name string, arguments []Expression) FunctionApplying {
-	return NewFunctionApplying(
-		arguments,
-		func(context context.Context, arguments []interface{}) (interface{}, error) {
-			function, ok := context.Value(name)
-			if !ok {
-				return nil, errors.Errorf("unknown function %s", name)
-			}
+func NewFunctionCall(name string, arguments []Expression) FunctionCall {
+	return FunctionCall{name, arguments}
+}
 
-			var reflectedArguments []reflect.Value
-			for _, argument := range arguments {
-				reflectedArguments = append(reflectedArguments, reflect.ValueOf(argument))
-			}
+// Evaluate ...
+func (expression FunctionCall) Evaluate(context context.Context) (result interface{}, err error) {
+	function, ok := context.Value(expression.name)
+	if !ok {
+		return nil, errors.Errorf("unknown function %s", expression.name)
+	}
 
-			var results []reflect.Value
-			var err error
-			func() {
-				defer func() {
-					if err2 := recover(); err2 != nil {
-						err = errors.Errorf("%v", err2)
-					}
-				}()
+	var arguments []reflect.Value
+	for index, argument := range expression.arguments {
+		result, err2 := argument.Evaluate(context)
+		if err2 != nil {
+			return nil, errors.Wrapf(
+				err2,
+				"unable to evaluate the argument %d for the function %s",
+				index,
+				expression.name,
+			)
+		}
 
-				results = reflect.ValueOf(function).Call(reflectedArguments)
-			}()
-			if err != nil {
-				return nil, errors.Wrapf(err, "unable to call the function %s", name)
-			}
-			if resultCount := len(results); resultCount != 1 {
-				return nil, errors.Errorf(
-					"incorrect number of results (%d) for the function %s",
-					resultCount,
-					name,
-				)
-			}
+		arguments = append(arguments, reflect.ValueOf(result))
+	}
 
-			return results[0].Interface(), nil
-		},
-	)
+	var results []reflect.Value
+	func() {
+		defer func() {
+			if err2 := recover(); err2 != nil {
+				err = errors.Errorf("%v", err2)
+			}
+		}()
+
+		results = reflect.ValueOf(function).Call(arguments)
+	}()
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to call the function %s", expression.name)
+	}
+	if resultCount := len(results); resultCount != 1 {
+		return nil, errors.Errorf(
+			"incorrect number of results (%d) for the function %s",
+			resultCount,
+			expression.name,
+		)
+	}
+
+	return results[0].Interface(), nil
 }
--- runtime/expressions/function_call_test.go
+++ runtime/expressions/function_call_test.go
@@ -2,14 +2,37 @@ package expressions
 
 import (
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/thewizardplusplus/tick-tock/runtime/context"
 	contextmocks "github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/expressions/mocks"
 )
 
+type SignedExpression struct {
+	*mocks.Expression
+
+	Sign string
+}
+
+func NewSignedExpression(sign string) SignedExpression {
+	return SignedExpression{new(mocks.Expression), sign}
+}
+
 func TestNewFunctionCall(test *testing.T) {
+	arguments := []Expression{NewSignedExpression("one"), NewSignedExpression("two")}
+	got := NewFunctionCall("test", arguments)
+
+	for _, argument := range arguments {
+		mock.AssertExpectationsForObjects(test, argument)
+	}
+	assert.Equal(test, "test", got.name)
+	assert.Equal(test, arguments, got.arguments)
+}
+
+func TestFunctionCall_Evaluate(test *testing.T) {
 	type fields struct {
 		name      string
 		arguments []Expression
@@ -60,6 +83,52 @@ func TestNewFunctionCall(test *testing.T) {
 		{
 			name: "error with an unknown function",
 			fields: fields{
+				name:      "add",
+				arguments: []Expression{NewSignedExpression("one"), NewSignedExpression("two")},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Value", "add").Return(nil, false)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error on argument evaluation",
+			fields: fields{
+				name: "add",
+				arguments: []Expression{
+					func() Expression {
+						expression := NewSignedExpression("one")
+						expression.
+							On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+							Return(nil, iotest.ErrTimeout)
+
+						return expression
+					}(),
+					NewSignedExpression("two"),
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					add := func(a float64, b float64) float64 { return a + b }
+
+					context := new(contextmocks.Context)
+					context.On("Value", "add").Return(add, true)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error on function calling (incorrect function type)",
+			fields: fields{
 				name: "add",
 				arguments: []Expression{
 					func() Expression {
@@ -79,7 +148,7 @@ func TestNewFunctionCall(test *testing.T) {
 			args: args{
 				context: func() context.Context {
 					context := new(contextmocks.Context)
-					context.On("Value", "add").Return(nil, false)
+					context.On("Value", "add").Return("incorrect", true)
 
 					return context
 				}(),
@@ -108,7 +177,39 @@ func TestNewFunctionCall(test *testing.T) {
 			},
 			args: args{
 				context: func() context.Context {
-					add := func(a float64, b float64) float64 { panic("not implemented") }
+					add := func(a float64, b float64) float64 { return a + b }
+
+					context := new(contextmocks.Context)
+					context.On("Value", "add").Return(add, true)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error on function calling (incorrect arguments count)",
+			fields: fields{
+				name: "add",
+				arguments: []Expression{
+					func() Expression {
+						expression := NewSignedExpression("one")
+						expression.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(2.3, nil)
+
+						return expression
+					}(),
+					func() Expression {
+						expression := NewSignedExpression("two")
+						expression.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(4.2, nil)
+
+						return expression
+					}(),
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					add := func(a float64, b float64, c float64) float64 { return a + b + c }
 
 					context := new(contextmocks.Context)
 					context.On("Value", "add").Return(add, true)
@@ -153,7 +254,10 @@ func TestNewFunctionCall(test *testing.T) {
 		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
-			expression := NewFunctionCall(data.fields.name, data.fields.arguments)
+			expression := FunctionCall{
+				name:      data.fields.name,
+				arguments: data.fields.arguments,
+			}
 			gotResult, gotErr := expression.Evaluate(data.args.context)
 
 			for _, argument := range data.fields.arguments {
