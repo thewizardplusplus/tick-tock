56c1d4dcb5805dff1e2e85d072667191285fda24
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-02-29 23:00:00 +0300

    Add the expressions.String structure

---
+++ runtime/expressions/string.go
@@ -0,0 +1,22 @@
+package expressions
+
+import (
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
+)
+
+// String ...
+type String struct {
+	value *types.Pair
+}
+
+// NewString ...
+func NewString(value string) String {
+	pair := types.NewPairFromString(value)
+	return String{pair}
+}
+
+// Evaluate ...
+func (expression String) Evaluate(context context.Context) (result interface{}, err error) {
+	return expression.value, nil
+}
---
+++ runtime/expressions/string_test.go
@@ -0,0 +1,30 @@
+package expressions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
+)
+
+func TestNewString(test *testing.T) {
+	got := NewString("hi")
+
+	want := &types.Pair{Head: 'h', Tail: &types.Pair{Head: 'i', Tail: nil}}
+	assert.Equal(test, want, got.value)
+}
+
+func TestString_Evaluate(test *testing.T) {
+	context := new(mocks.Context)
+	string := String{
+		value: &types.Pair{Head: 'h', Tail: &types.Pair{Head: 'i', Tail: nil}},
+	}
+	gotResult, gotErr := string.Evaluate(context)
+
+	mock.AssertExpectationsForObjects(test, context)
+	wantResult := &types.Pair{Head: 'h', Tail: &types.Pair{Head: 'i', Tail: nil}}
+	assert.Equal(test, wantResult, gotResult)
+	assert.NoError(test, gotErr)
+}
