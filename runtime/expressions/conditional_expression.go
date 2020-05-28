082e8efd63a4a429b794f365a7b59d88796aed9f
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-05-28 23:00:00 +0300

    Add the expressions.ConditionalExpression structure

---
+++ runtime/expressions/conditional_expression.go
@@ -0,0 +1,54 @@
+package expressions
+
+import (
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/tick-tock/runtime"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
+)
+
+// ConditionalCase ...
+type ConditionalCase struct {
+	Condition Expression
+	Commands  runtime.CommandGroup
+}
+
+// ConditionalExpression ...
+type ConditionalExpression struct {
+	conditionalCases []ConditionalCase
+}
+
+// NewConditionalExpression ...
+func NewConditionalExpression(conditionalCases []ConditionalCase) ConditionalExpression {
+	return ConditionalExpression{conditionalCases}
+}
+
+// Evaluate ...
+func (expression ConditionalExpression) Evaluate(
+	context context.Context,
+) (result interface{}, err error) {
+	for conditionalCaseIndex, conditionalCase := range expression.conditionalCases {
+		contextCopy := context.Copy()
+		conditionResult, err := conditionalCase.Condition.Evaluate(contextCopy)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unable to evaluate the condition #%d", conditionalCaseIndex)
+		}
+
+		conditionBooleanResult, err := types.NewBoolean(conditionResult)
+		if err != nil {
+			return nil,
+				errors.Wrapf(err, "unable to convert the condition #%d to boolean", conditionalCaseIndex)
+		}
+		if conditionBooleanResult == types.True {
+			commandsResult, err := conditionalCase.Commands.Run(contextCopy)
+			if err != nil {
+				return nil,
+					errors.Wrapf(err, "unable to evaluate commands of the condition #%d", conditionalCaseIndex)
+			}
+
+			return commandsResult, nil
+		}
+	}
+
+	return types.Nil{}, nil
+}
