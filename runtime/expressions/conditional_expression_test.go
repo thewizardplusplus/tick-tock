e2404d6d08fc5128e05a74b6bc8b6b747bb3478b
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-05-29 23:00:00 +0300

    Complete tests for the expressions.ConditionalExpression structure

--- runtime/expressions/conditional_expression_test.go
+++ runtime/expressions/conditional_expression_test.go
@@ -2,21 +2,25 @@ package expressions
 
 import (
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/tick-tock/runtime"
 	"github.com/thewizardplusplus/tick-tock/runtime/context"
-	"github.com/thewizardplusplus/tick-tock/runtime/mocks"
+	contextmocks "github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	runtimemocks "github.com/thewizardplusplus/tick-tock/runtime/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
 )
 
 type SignedCommand struct {
-	*mocks.Command
+	*runtimemocks.Command
 
 	Sign string
 }
 
 func NewSignedCommand(sign string) SignedCommand {
-	return SignedCommand{new(mocks.Command), sign}
+	return SignedCommand{new(runtimemocks.Command), sign}
 }
 
 func TestNewConditionalExpression(test *testing.T) {
@@ -45,7 +49,249 @@ func TestConditionalExpression_Evaluate(test *testing.T) {
 		wantResult interface{}
 		wantErr    assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success/without conditions",
+			fields: fields{
+				conditionalCases: nil,
+			},
+			args: args{
+				context: new(contextmocks.Context),
+			},
+			wantResult: types.Nil{},
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "success/with conditions/with match",
+			fields: fields{
+				conditionalCases: []ConditionalCase{
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("one-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("one-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("two-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.True, nil)
+
+							return expression
+						}(),
+						Command: func() runtime.Command {
+							command := NewSignedCommand("two-command")
+							command.On("Run", mock.AnythingOfType("*mocks.Context")).Return(23.0, nil)
+
+							return command
+						}(),
+					},
+					{
+						Condition: NewSignedExpression("three-condition"),
+						Command:   NewSignedCommand("three-command"),
+					},
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Copy").Return(context)
+
+					return context
+				}(),
+			},
+			wantResult: 23.0,
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "success/with conditions/without match",
+			fields: fields{
+				conditionalCases: []ConditionalCase{
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("one-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("one-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("two-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("two-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("three-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("three-command"),
+					},
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Copy").Return(context)
+
+					return context
+				}(),
+			},
+			wantResult: types.Nil{},
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "error/with condition evaluation",
+			fields: fields{
+				conditionalCases: []ConditionalCase{
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("one-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("one-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("two-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(nil, iotest.ErrTimeout)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("two-command"),
+					},
+					{
+						Condition: NewSignedExpression("three-condition"),
+						Command:   NewSignedCommand("three-command"),
+					},
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Copy").Return(context)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error/with condition conversion",
+			fields: fields{
+				conditionalCases: []ConditionalCase{
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("one-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("one-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("two-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(func() {}, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("two-command"),
+					},
+					{
+						Condition: NewSignedExpression("three-condition"),
+						Command:   NewSignedCommand("three-command"),
+					},
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Copy").Return(context)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error/with command running",
+			fields: fields{
+				conditionalCases: []ConditionalCase{
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("one-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.False, nil)
+
+							return expression
+						}(),
+						Command: NewSignedCommand("one-command"),
+					},
+					{
+						Condition: func() Expression {
+							expression := NewSignedExpression("two-condition")
+							expression.
+								On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+								Return(types.True, nil)
+
+							return expression
+						}(),
+						Command: func() runtime.Command {
+							command := NewSignedCommand("two-command")
+							command.On("Run", mock.AnythingOfType("*mocks.Context")).Return(nil, iotest.ErrTimeout)
+
+							return command
+						}(),
+					},
+					{
+						Condition: NewSignedExpression("three-condition"),
+						Command:   NewSignedCommand("three-command"),
+					},
+				},
+			},
+			args: args{
+				context: func() context.Context {
+					context := new(contextmocks.Context)
+					context.On("Copy").Return(context)
+
+					return context
+				}(),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			expression := ConditionalExpression{
