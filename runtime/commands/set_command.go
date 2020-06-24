7c01207f60b34b4c940dcab63b11a9b3013532dc
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-06-24 23:00:00 +0300

    Fix the commands.SetCommand structure

--- runtime/commands/set_command.go
+++ runtime/commands/set_command.go
@@ -1,24 +1,44 @@
 package commands
 
 import (
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/runtime/expressions"
 	"github.com/thewizardplusplus/tick-tock/runtime/types"
 )
 
 // SetCommand ...
 type SetCommand struct {
-	state string
+	name      string
+	arguments []expressions.Expression
 }
 
 // NewSetCommand ...
-func NewSetCommand(state string) SetCommand {
-	return SetCommand{state}
+func NewSetCommand(name string, arguments []expressions.Expression) SetCommand {
+	return SetCommand{name, arguments}
 }
 
 // Run ...
-func (command SetCommand) Run(context context.Context) (result interface{}, err error) {
-	if err := context.SetState(command.state); err != nil {
-		return nil, err
+func (command SetCommand) Run(ctx context.Context) (result interface{}, err error) {
+	var arguments []interface{}
+	for index, argument := range command.arguments {
+		result, err := argument.Evaluate(ctx)
+		if err != nil {
+			return nil, errors.Wrapf(
+				err,
+				"unable to evaluate the argument #%d for the set command",
+				index,
+			)
+		}
+
+		arguments = append(arguments, result)
+	}
+
+	if err := ctx.SetState(context.State{
+		Name:      command.name,
+		Arguments: arguments,
+	}); err != nil {
+		return nil, errors.Wrapf(err, "unable to set the state %s", command.name)
 	}
 
 	return types.Nil{}, nil
--- runtime/commands/set_command_test.go
+++ runtime/commands/set_command_test.go
@@ -6,37 +6,138 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	contextmocks "github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/expressions"
+	expressionsmocks "github.com/thewizardplusplus/tick-tock/runtime/expressions/mocks"
 	"github.com/thewizardplusplus/tick-tock/runtime/types"
 )
 
 func TestSetCommand(test *testing.T) {
+	type fields struct {
+		name      string
+		arguments []expressions.Expression
+	}
+	type args struct {
+		context context.Context
+	}
+
 	for _, testData := range []struct {
 		name       string
-		settingErr error
+		fields     fields
+		args       args
 		wantResult interface{}
 		wantErr    assert.ErrorAssertionFunc
 	}{
 		{
-			name:       "success",
-			settingErr: nil,
+			name: "success without arguments",
+			fields: fields{
+				name:      "test",
+				arguments: nil,
+			},
+			args: args{
+				context: func() context.Context {
+					state := context.State{Name: "test"}
+
+					context := new(contextmocks.Context)
+					context.On("SetState", state).Return(nil)
+
+					return context
+				}(),
+			},
 			wantResult: types.Nil{},
 			wantErr:    assert.NoError,
 		},
 		{
-			name:       "error",
-			settingErr: iotest.ErrTimeout,
+			name: "success with arguments",
+			fields: fields{
+				name: "test",
+				arguments: func() []expressions.Expression {
+					expressionOne := new(expressionsmocks.Expression)
+					expressionOne.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(2.3, nil)
+
+					expressionTwo := new(expressionsmocks.Expression)
+					expressionTwo.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(4.2, nil)
+
+					return []expressions.Expression{expressionOne, expressionTwo}
+				}(),
+			},
+			args: args{
+				context: func() context.Context {
+					state := context.State{
+						Name:      "test",
+						Arguments: []interface{}{2.3, 4.2},
+					}
+
+					context := new(contextmocks.Context)
+					context.On("SetState", state).Return(nil)
+
+					return context
+				}(),
+			},
+			wantResult: types.Nil{},
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "error with argument evaluation",
+			fields: fields{
+				name: "test",
+				arguments: func() []expressions.Expression {
+					expressionOne := new(expressionsmocks.Expression)
+					expressionOne.
+						On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+						Return(nil, iotest.ErrTimeout)
+
+					expressionTwo := new(expressionsmocks.Expression)
+
+					return []expressions.Expression{expressionOne, expressionTwo}
+				}(),
+			},
+			args: args{
+				context: new(contextmocks.Context),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error with state setting",
+			fields: fields{
+				name: "test",
+				arguments: func() []expressions.Expression {
+					expressionOne := new(expressionsmocks.Expression)
+					expressionOne.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(2.3, nil)
+
+					expressionTwo := new(expressionsmocks.Expression)
+					expressionTwo.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(4.2, nil)
+
+					return []expressions.Expression{expressionOne, expressionTwo}
+				}(),
+			},
+			args: args{
+				context: func() context.Context {
+					state := context.State{
+						Name:      "test",
+						Arguments: []interface{}{2.3, 4.2},
+					}
+
+					context := new(contextmocks.Context)
+					context.On("SetState", state).Return(iotest.ErrTimeout)
+
+					return context
+				}(),
+			},
 			wantResult: nil,
 			wantErr:    assert.Error,
 		},
 	} {
 		test.Run(testData.name, func(test *testing.T) {
-			context := new(mocks.Context)
-			context.On("SetState", "test").Return(testData.settingErr)
+			gotResult, gotErr := NewSetCommand(testData.fields.name, testData.fields.arguments).
+				Run(testData.args.context)
 
-			gotResult, gotErr := NewSetCommand("test").Run(context)
-
-			mock.AssertExpectationsForObjects(test, context)
+			mock.AssertExpectationsForObjects(test, testData.args.context)
+			for _, argument := range testData.fields.arguments {
+				mock.AssertExpectationsForObjects(test, argument)
+			}
 			assert.Equal(test, testData.wantResult, gotResult)
 			testData.wantErr(test, gotErr)
 		})
