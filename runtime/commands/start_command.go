ea82ac989e79d8d584aa563cc1aea3353894e860
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-10 23:00:00 +0300

    Add the commands.StartCommand structure

---
+++ runtime/commands/start_command.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/tick-tock/runtime"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	"github.com/thewizardplusplus/tick-tock/runtime/expressions"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
+)
+
+// StartCommand ...
+type StartCommand struct {
+	actorFactory expressions.Expression
+}
+
+// NewStartCommand ...
+func NewStartCommand(actorFactory expressions.Expression) StartCommand {
+	return StartCommand{actorFactory}
+}
+
+// Run ...
+func (command StartCommand) Run(context context.Context) (result interface{}, err error) {
+	actorFactory, err := command.actorFactory.Evaluate(context)
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to evaluate the actor class for the start command")
+	}
+
+	typedActorFactory, ok := actorFactory.(runtime.ConcurrentActorFactory)
+	if !ok {
+		return nil, errors.Errorf(
+			"unsupported type %T of the actor class for the start command",
+			actorFactory,
+		)
+	}
+
+	actor := typedActorFactory.CreateActor()
+	context.RegisterActor(actor)
+
+	return types.Nil{}, nil
+}
---
+++ runtime/commands/start_command_test.go
@@ -0,0 +1,139 @@
+package commands
+
+import (
+	"reflect"
+	"testing"
+	"testing/iotest"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	testutils "github.com/thewizardplusplus/tick-tock/internal/test-utils"
+	"github.com/thewizardplusplus/tick-tock/runtime"
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+	contextmocks "github.com/thewizardplusplus/tick-tock/runtime/context/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/expressions"
+	expressionsmocks "github.com/thewizardplusplus/tick-tock/runtime/expressions/mocks"
+	"github.com/thewizardplusplus/tick-tock/runtime/types"
+)
+
+func TestStartCommand(test *testing.T) {
+	type fields struct {
+		actorFactory expressions.Expression
+	}
+	type args struct {
+		context context.Context
+	}
+
+	for _, testData := range []struct {
+		name       string
+		fields     fields
+		args       args
+		wantResult interface{}
+		wantErr    assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			fields: fields{
+				actorFactory: func() expressions.Expression {
+					actorFactory, _ := runtime.NewActorFactory(
+						"Test",
+						runtime.StateGroup{
+							"state_0": runtime.ParameterizedMessageGroup{},
+							"state_1": runtime.ParameterizedMessageGroup{},
+						},
+						context.State{Name: "state_0"},
+					)
+					concurrentActorFactory := runtime.NewConcurrentActorFactory(
+						actorFactory,
+						testutils.UnbufferedInbox,
+						runtime.Dependencies{},
+					)
+
+					expression := new(expressionsmocks.Expression)
+					expression.
+						On("Evaluate", mock.AnythingOfType("*mocks.Context")).
+						Return(concurrentActorFactory, nil)
+
+					return expression
+				}(),
+			},
+			args: args{
+				context: func() context.Context {
+					actorFactory, _ := runtime.NewActorFactory(
+						"Test",
+						runtime.StateGroup{
+							"state_0": runtime.ParameterizedMessageGroup{},
+							"state_1": runtime.ParameterizedMessageGroup{},
+						},
+						context.State{Name: "state_0"},
+					)
+					concurrentActorFactory := runtime.NewConcurrentActorFactory(
+						actorFactory,
+						testutils.UnbufferedInbox,
+						runtime.Dependencies{},
+					)
+					wantActor := concurrentActorFactory.CreateActor()
+					cleanInbox(&wantActor)
+
+					context := new(contextmocks.Context)
+					context.
+						On("RegisterActor", mock.MatchedBy(func(gotActor runtime.ConcurrentActor) bool {
+							cleanInbox(&gotActor)
+							return reflect.DeepEqual(wantActor, gotActor)
+						})).
+						Return()
+
+					return context
+				}(),
+			},
+			wantResult: types.Nil{},
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "error with actor class evaluation",
+			fields: fields{
+				actorFactory: func() expressions.Expression {
+					expression := new(expressionsmocks.Expression)
+					expression.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(nil, iotest.ErrTimeout)
+
+					return expression
+				}(),
+			},
+			args: args{
+				context: new(contextmocks.Context),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error with an incorrect actor class type",
+			fields: fields{
+				actorFactory: func() expressions.Expression {
+					expression := new(expressionsmocks.Expression)
+					expression.On("Evaluate", mock.AnythingOfType("*mocks.Context")).Return(2.3, nil)
+
+					return expression
+				}(),
+			},
+			args: args{
+				context: new(contextmocks.Context),
+			},
+			wantResult: nil,
+			wantErr:    assert.Error,
+		},
+	} {
+		test.Run(testData.name, func(test *testing.T) {
+			gotResult, gotErr := NewStartCommand(testData.fields.actorFactory).Run(testData.args.context)
+
+			mock.AssertExpectationsForObjects(test, testData.args.context)
+			assert.Equal(test, testData.wantResult, gotResult)
+			testData.wantErr(test, gotErr)
+		})
+	}
+}
+
+func cleanInbox(actor *runtime.ConcurrentActor) {
+	inboxField := reflect.ValueOf(actor).Elem().FieldByName("inbox")
+	*(*chan string)(unsafe.Pointer(inboxField.UnsafeAddr())) = nil
+}
