d86b0aee79c79d52c64101b29427989eebc91e2c
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-18 23:00:00 +0300

    Add the types.getActorClassName() function

--- runtime/types/factory.go
+++ runtime/types/factory.go
@@ -8,3 +8,9 @@ func isActorClass(value interface{}) bool {
 	// can't use this type directly because it occurs an import cycle
 	return reflect.TypeOf(value).Name() == "ConcurrentActorFactory"
 }
+
+func getActorClassName(actorClass interface{}) string {
+	// can't use this type directly because it occurs an import cycle
+	results := reflect.ValueOf(actorClass).MethodByName("Name").Call(nil)
+	return results[0].Interface().(string)
+}
