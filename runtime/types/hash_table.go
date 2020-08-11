1d46f3aee67c21ea2c5ad679e51bbc9d9069a6aa
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-11 23:00:00 +0300

    Add the types.prepareKey() function

---
+++ runtime/types/hash_table.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"github.com/pkg/errors"
+)
+
+func prepareKey(key interface{}) (interface{}, error) {
+	switch typedKey := key.(type) {
+	case Nil, float64:
+		return typedKey, nil
+	case *Pair:
+		keyAsString, err := typedKey.Text()
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to convert the key to a string")
+		}
+
+		return keyAsString, nil
+	default:
+		return nil, errors.Errorf("unsupported type %T of the key for the hash table", key)
+	}
+}
