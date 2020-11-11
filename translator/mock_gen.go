55f94fda17ce9cec85f48d409cf928b2cb5562b8
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-11 23:00:00 +0300

    Generate a mock for the runtime.ErrorHandler interface in the translator package

--- translator/mock_gen.go
+++ translator/mock_gen.go
@@ -2,6 +2,7 @@ package translator
 
 import (
 	syncutils "github.com/thewizardplusplus/go-sync-utils"
+	"github.com/thewizardplusplus/tick-tock/runtime"
 )
 
 //go:generate mockery -name=Waiter -inpkg -case=underscore -testonly
@@ -12,4 +13,14 @@ import (
 //
 type Waiter interface {
 	syncutils.WaitGroup
+}
+
+//go:generate mockery -name=ErrorHandler -inpkg -case=underscore -testonly
+
+// ErrorHandler ...
+//
+// It's used only for mock generating.
+//
+type ErrorHandler interface {
+	runtime.ErrorHandler
 }
