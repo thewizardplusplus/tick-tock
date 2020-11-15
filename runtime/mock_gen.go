d30a4f11daff623a0c83753e2e19fa1f7f4d2b43
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-15 23:00:00 +0300

    Generate a mock for the context.Context interface in the runtime package

---
+++ runtime/mock_gen.go
@@ -0,0 +1,15 @@
+package runtime
+
+import (
+	"github.com/thewizardplusplus/tick-tock/runtime/context"
+)
+
+//go:generate mockery -name=Context -inpkg -case=underscore -testonly
+
+// Context ...
+//
+// It's used only for mock generating.
+//
+type Context interface {
+	context.Context
+}
