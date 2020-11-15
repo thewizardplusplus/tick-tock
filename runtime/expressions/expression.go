b0255161c4a9957acb8e5d10606b0e34672ad401
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-15 23:00:00 +0300

    Generate a mock for the expressions.Expression interface in the expressions package

--- runtime/expressions/expression.go
+++ runtime/expressions/expression.go
@@ -4,8 +4,9 @@ import (
 	"github.com/thewizardplusplus/tick-tock/runtime/context"
 )
 
+//go:generate mockery -name=Expression -inpkg -case=underscore -testonly
+
 // Expression ...
-//go:generate mockery -name=Expression -case=underscore
 type Expression interface {
 	Evaluate(context context.Context) (result interface{}, err error)
 }
