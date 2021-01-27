7dfc1561ef62a5cd8b3f8edebecd976f620541a6
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-27 23:00:00 +0300

    Add an explanatory comment for the parser.SetInnerField() function

--- parser/set_inner_field.go
+++ parser/set_inner_field.go
@@ -5,6 +5,18 @@ import (
 )
 
 // SetInnerField ...
+//
+// It creates a hierarchy of structures and sets the specified field.
+//
+// First, it searches the specified field in the passed root value. If the field was found, then
+// the specified value is set to it.
+//
+// Otherwise, the first field is selected and filled with a zero value of the corresponding type.
+// After that, the search recursively continues in it with the same logic.
+//
+// The passed root value and the first field on each step of the search should be a pointer
+// to a structure.
+//
 func SetInnerField(rootValue interface{}, fieldName string, fieldValue interface{}) interface{} {
 	value := reflect.ValueOf(rootValue).Elem()
 	for {
